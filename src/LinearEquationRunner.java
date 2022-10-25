9622d8ea759672138fee02f25fcd796a166162af
student <sample@bths.edu>
2022-10-25 13:18:02 -0400

    added print statements

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -18,9 +18,22 @@ public class LinearEquationRunner {
         int x2 = Integer.parseInt(coord2.substring(coord2.indexOf("(") + 1, coord2.indexOf(",")));
         int y2 = Integer.parseInt(coord2.substring(coord2.indexOf(", ") + 2, coord2.indexOf(")")));
 
-        // creating LinearEquation object
-        LinearEquation lineareq = new LinearEquation(x1, y1, x2, y2);
-        System.out.println(lineareq.lineInfo());
+        // creating LinearEquation object and prompting the user to enter an x value (only if the x coordinates are not equal)
+        if (x1 == x2){
+            System.out.println("The points are on a vertical line: x = " + x1);
+        } else {
+            LinearEquation lineareq = new LinearEquation(x1, y1, x2, y2);
+            System.out.println("");
+            System.out.println(lineareq.lineInfo());
+            System.out.println("");
+            System.out.print("Enter a value for x: ");
+            int xCoord = scan.nextInt();
+            scan.nextLine();
+            System.out.println("");
+            System.out.println("The point on the line is " + lineareq.coordinateForX(xCoord));
+
+        }
+
 
 
 
